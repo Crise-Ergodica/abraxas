e6740881968cb36d35231fb4a46bf0a80245205b
Aurora <gdcm10@gmail.com>
2025-12-12 12:13:10 -0300

    Criando o necessário para atualizar o deploy sempre que um push for dado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  // SUBSTITUA PELA SUA URL DO GITHUB
+  // Exemplo: https://aurora.github.io (sem a subpasta aqui)
+  site: 'crise-ergodica.github.io',
+
+  // O nome do seu repositório (a subpasta)
+  // Se o seu repo chama "abraxas", mantenha assim.
+  base: '/abraxas',
+
+  integrations: [mdx()],
 });
